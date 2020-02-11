30578529d379dce7de96016135bcde11f7ab07d9
jhenvi <54624339+jhenvi@users.noreply.github.com>
2020-02-10 20:49:16 -0500

    Uploading DB work

---
+++ resources/raw_data2.sql
@@ -0,0 +1,12 @@
+CREATE TABLE raw_data (
+	Sector VARCHAR(120),
+	Indicator VARCHAR(120),
+	Unit VARCHAR(120),
+	Country_code VARCHAR(120),
+	Country VARCHAR(120),
+	Year VARCHAR(120),
+	Variable VARCHAR(120),
+	Value VARCHAR(120)
+);
+
+select * from raw_data
