46d2a3a9bf66d23132da0c28c5ed001eb9ea2b26
jbersinic <54554895+jbersinic@users.noreply.github.com>
2020-02-15 15:36:06 -0500

    updating
    - added hero image
    - updated issues with code for small screen (nav)

--- Emissions/app.py
+++ Emissions/app.py
@@ -10,7 +10,7 @@ from flask import (
 #################################################
 # Flask Setup
 #################################################
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 #################################################
 # Database Setup
